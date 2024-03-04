f160855805d2a1524af76ef7268fe05a426b3384
Zuleima <zuleima2304@gmail.com>
2024-03-04 00:10:38 +0000

    - Añadimos las carpetas css y javascript y movemos los archivos a cada carpeta - Terminamos el css de inscription y modificamos su html - Creamos un .js de bookings

---
+++ javascript/bookings.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    // Obtener el parámetro "clase" de la URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var clase = urlParams.get('clase');
+    if (clase) {
+        // Seleccionar automáticamente el botón correspondiente
+        var botonClase = document.querySelector('button[data-clase="' + clase + '"]');
+        if (botonClase) {
+            reservarClase(clase); // Llamar a la función para resaltar el botón
+        }
+    }
+}
+
+function reservarClase(clase) {
+    // Restablecer el color de fondo de todos los botones
+    var botones = document.querySelectorAll('button');
+    botones.forEach(function(boton) {
+        boton.style.backgroundColor = '#f9f9f9';
+    });
+    // Cambiar el color de fondo del botón seleccionado
+    var botonSeleccionado = document.querySelector('button[data-clase="' + clase + '"]');
+    if (botonSeleccionado) {
+        botonSeleccionado.style.backgroundColor = '#007bff';
+    }
+    document.getElementById('clase').value = clase;
+}
