79f81dd3caf05b7b3b27faeb440676a13b7dc85f
Zuleima <zuleima2304@gmail.com>
2024-03-06 22:40:43 +0000

    - Creamos los js que faltaban (form, home, inscription, timetable) - Invertimos los colores en todos los css

--- javascript/bookings.js
+++ javascript/bookings.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('header.html')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('body').insertAdjacentHTML('beforebegin',html);
+        })
+        .catch(error => {
+            console.error('Error al cargar el header: ', error)
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('body').insertAdjacentHTML('beforeend', html);
+        })
+        .catch(error => {
+            console.error('Error al cargar el footer:', error);
+        });
+});
+
 window.onload = function() {
     // Obtener el parámetro "clase" de la URL
     var urlParams = new URLSearchParams(window.location.search);
