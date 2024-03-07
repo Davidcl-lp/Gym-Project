1f3f5c438dc140c701aadd84303c430e8f381093
Davidcl-lp <dclworker@gmail.com>
2024-03-07 14:58:11 +0000

    Todo terminado menos pagos

--- javascript/account.js
+++ javascript/account.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('footer.html')
+    fetch('./footer.html')
     .then(response => response.text())
     .then(html => {
       document.querySelector('body').insertAdjacentHTML('beforeend', html);
