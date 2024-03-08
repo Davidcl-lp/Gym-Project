be960b9d35073d0b81aa44a88214cf4f4fb9f29c
Zuleima <zuleima2304@gmail.com>
2024-03-08 21:27:57 +0000

    - Arreglado el problema de las fuentes del header - Arreglado el html y css de registrationpayment y paymethods - Añadido el footer en paymethods y registrationpayment - Pediente de arreglar timetable, myhome y agregar fotos en services

--- javascript/paymethods.js
+++ javascript/paymethods.js
@@ -8,3 +8,14 @@ document.addEventListener("DOMContentLoaded",function(){
         console.error('Error al cargar el header: ', error)
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('body').insertAdjacentHTML('beforeend', html);
+        })
+        .catch(error => {
+            console.error('Error al cargar el footer:', error);
+        });
+});
